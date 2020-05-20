ecb41df7e9167e42bf3619a2f51caa534be72e03
Iona <ionabrabender@gmail.com>
2020-05-20 18:09:36 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,14 +29,10 @@ def play(songs)
   user_response = gets.strip
   if user_response.to_i > 0 && user_response.to_i <= songs.length
       puts "Playing #{songs[(user_response.to_i - 1)]}"
-  else 
-    songs.each do |song_title|
-      if songs.include?(user_response)
-        puts "Playing #{user_response}"
-      else
-        puts "Invalid input, please try again"
-      end
-    
+  elsif songs.include?(user_response)
+      puts "Playing #{user_response}"
+  else
+      puts "Invalid input, please try again"
   end
 end
 
