6f096ef256141e512430cda9ee1297c30605dc7a
Iona <ionabrabender@gmail.com>
2020-05-20 17:59:26 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,7 @@ def play(songs)
   binding.pry
     if song_title == user_response
       puts "Playing #{user_response}"
-    elsif user_response.to_i > 0 && user_response.to_i < songs.length
+    elsif user_response.to_i > 0 && user_response.to_i <= songs.length
       puts "Playing #{songs[(user_response.to_i - 1)]}"
     end
     #binding.pry
